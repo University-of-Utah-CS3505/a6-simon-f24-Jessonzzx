b9594ec9e4230db0124a3f063d7ddfbc57724272
Zhengxi Zhang <u1341522@umail.utah.edu>
2024-10-29 16:29:13 -0600

    finish the basic setup.

---
+++ A6/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "model.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void handleDisplaySequence(int color, int delay);
+    void handleProgressUpdate(int progress);
+    void handleGameOver(bool won);
+    void updateScore(int newScore);
+
+private:
+    Ui::MainWindow *ui;
+    model *model;
+
+    void resetUI();
+    void enablePlayerButtons(bool enable);
+};
+#endif // MAINWINDOW_H
