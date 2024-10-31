c85386e4f43c895d8c57fac58128a13c8dc8b7d1
Zhengxi Zhang <u1341522@umail.utah.edu>
2024-10-30 20:51:25 -0600

    removed not use files.

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_A6_FILE E:/CS3505/a6-simon-f24-Jessonzzx/A6/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/A6.exe)
-set(__QT_DEPLOY_TARGET_A6_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_A6_RUNTIME_DLLS E:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;E:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;E:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
